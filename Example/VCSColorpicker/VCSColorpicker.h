2d59e2aadd9d6735a1dd3405ac75dd99b173afd3
iiliev <iiliev@nemetschek.bg>
2018-11-08 15:47:15 +0200

    Add missing files

---
+++ Example/VCSColorpicker/VCSColorpicker.h
@@ -0,0 +1,25 @@
+//
+//  VCSColorpicker1.h
+//  VCSColorpicker1
+//
+//  Created by Ivaylo Iliev on 8.11.18.
+//  Copyright © 2018 Karthik Abram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VCSColorpicker1.
+FOUNDATION_EXPORT double VCSColorpicker1VersionNumber;
+
+//! Project version string for VCSColorpicker1.
+FOUNDATION_EXPORT const unsigned char VCSColorpicker1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VCSColorpicker1/PublicHeader.h>
+
+#import <VCSColorpicker/NEOColorPickerFavoritesViewController.h>
+#import <VCSColorpicker/NEOColorPickerFavoritesManager.h>
+#import <VCSColorpicker/NEOColorPickerGradientView.h>
+#import <VCSColorpicker/NEOColorPickerHSLViewController.h>
+#import <VCSColorpicker/NEOColorPickerHueGridViewController.h>
+#import <VCSColorpicker/NEOColorPickerViewController.h>
+#import <VCSColorpicker/UIColor+NEOColor.h>
